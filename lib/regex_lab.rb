5f80ad3142f186c75c43227c128253178d70fac5
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-07 22:48:14 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  word.match(/\b[A-Z]\w+\./) ? true : false
 end
 
 def valid_phone_number?(phone)
